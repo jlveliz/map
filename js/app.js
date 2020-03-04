6f45280021db32eb49558857ea7d29ab97448bc2
Jorge Veliz <jorge.veliz@guayas.gob.ec>
2020-03-04 12:07:06 -0500

    feat(fetch): implementa datos desde datos .json

--- js/app.js
+++ js/app.js
@@ -4,7 +4,66 @@
  *
  */
 
-const AppName = "Guayas Contigo"; 
+const AppName = "Guayas Contigo";
+
+const Url = '../data/data.json';
+
+
+async function getData(id) {
+    let request = new Request(Url)
+    let data = await fetch(Url).then(response => response.json()).then(data => data)
+    return data.find(item => item.id == id)
+}
+
+const setInfoData = data => {
+    let list = document.createElement('ul');
+    
+    //Asistentes
+    let listAsistentesNode = document.createElement('li');
+    let strongNodeAsistentes = document.createElement('strong');
+    let textStrong = document.createTextNode("Asistentes: " )
+    strongNodeAsistentes.appendChild(textStrong)
+    let asistentesText = document.createTextNode(data.asistentes)
+    listAsistentesNode.appendChild(strongNodeAsistentes)
+    listAsistentesNode.appendChild(asistentesText)
+    list.appendChild(listAsistentesNode)
+
+    //Pacientes
+    let listPacientesNode = document.createElement('li');
+    let strongNodePacientes = document.createElement('strong');
+    textStrongPacientes = document.createTextNode("Pacientes: " )
+    strongNodePacientes.appendChild(textStrongPacientes)
+    PacientesText = document.createTextNode(data.pacientes)
+    listPacientesNode.appendChild(strongNodePacientes)
+    listPacientesNode.appendChild(PacientesText)
+    list.appendChild(listPacientesNode)
+
+
+    //Medicinas
+    let listMedicinasNode = document.createElement('li');
+    let strongNodeMedicinas = document.createElement('strong');
+    textStrongMedicinas = document.createTextNode("Medicinas: " )
+    strongNodeMedicinas.appendChild(textStrongMedicinas)
+    let medicinasText = document.createTextNode(data.medicinas)
+    listMedicinasNode.appendChild(strongNodeMedicinas)
+    listMedicinasNode.appendChild(medicinasText)
+    list.appendChild(listMedicinasNode)
+    
+    //Mascotas
+    let listMascotasNode = document.createElement('li');
+    let strongNodeMascotas = document.createElement('strong');
+    let textStrongMascotas = document.createTextNode("Mascotas: " )
+    let mascotasText = document.createTextNode(data.mascotas)
+    strongNodeMascotas.appendChild(textStrongMascotas)
+    listMascotasNode.appendChild(strongNodeMascotas)
+    listMascotasNode.appendChild(mascotasText)
+    list.appendChild(listMascotasNode)
+
+
+    
+
+    return list;
+}
 
 const changeFillPolygon = (element, event) => {
     const fillColor = event == 'mouseover' ? "024693" : "0095D6";
@@ -35,14 +94,20 @@ const hideTooltip = e => {
     tooltipTag.setAttribute('visibility', 'hidden')
 }
 
-const showModalInfo = e => {
-
+async function showModalInfo(e) {
     const modal = document.getElementById('modalInfo')
-    modal.style.display = "block";
     let cityTitle = e.target.getAttribute('title');
-    const modalTitle = document.querySelector('.modal-title')
-    if(cityTitle)
-        modalTitle.innerHTML=  AppName+ ' ' + cityTitle
+    let idCity = e.target.getAttribute('id');
+    let data = await getData(idCity);
+
+    if (cityTitle) {
+        const modalTitle = document.querySelector('.modal-title')
+        const modalContent = document.querySelector('.modal-body')
+        modal.style.display = "block";
+        modalTitle.innerHTML = AppName + ' ' + cityTitle
+        modalContent.innerHTML = '';
+        modalContent.appendChild(setInfoData(data));
+    }
 }
 
 
@@ -71,20 +136,28 @@ document.onreadystatechange = e => {
 
 
         //tooltip
-        var polygonsCities = document.querySelectorAll('polygon');
-        for (let index = 0; index < polygonsCities.length; index++) {
-            polygonsCities[index].addEventListener('mouseover', showTooltip);
-            polygonsCities[index].addEventListener('mouseout', hideTooltip);
-            //open modal
-            polygonsCities[index].addEventListener('click', showModalInfo);
+        var polygonsTooltips = document.querySelectorAll('polygon');
+
+        for (let index = 0; index < polygonsTooltips.length; index++) {
+            polygonsTooltips[index].addEventListener('mouseover', showTooltip);
+            polygonsTooltips[index].addEventListener('mouseout', hideTooltip);
+
         }
+
+        var polygonsCitiesOpenModal = document.querySelectorAll('polygon.visited, polygon.next');
+        for (let index = 0; index < polygonsCitiesOpenModal.length; index++) {
+            //open modal
+            polygonsCitiesOpenModal[index].addEventListener('click', showModalInfo);
+
+        }
+
 
 
 
         let spanClose = document.querySelectorAll('.close')
         for (let index = 0; index < spanClose.length; index++) {
             spanClose[index].addEventListener('click', closeDivModal)
-            
+
         }
         window.addEventListener('click', closeModalWindow)
 
